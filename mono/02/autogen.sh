c527ddf1fd00b8e79f7f3d06471fbdee346f16b0
Ivan Lopes <lopesivan.ufrj@gmail.com>
2023-03-21 00:05:14 -0300

    Atualiza o arquivo mono/02/autogen.sh

--- mono/02/autogen.sh
+++ mono/02/autogen.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env sh
-#
+
 #aclocal && autoconf && automake --add-missing
 #
 #touch NEWS README AUTHORS ChangeLog COPYING
-autoreconf -i -v && ./configure && make
-#
-#exit 0
+autoreconf -i -v && 
+    ./configure && 
+    make &&
+    make -C src/ run
+
+exit 0
