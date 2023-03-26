9b5b1fa2efbbde788c32ba6b067ca9dd8436868b
Ivan Lopes <lopesivan.ufrj@gmail.com>
2023-03-26 18:01:06 -0300

    Atualiza o arquivo ssh/01/README.md

--- ssh/01/README.md
+++ ssh/01/README.md
@@ -1,4 +1,3 @@
-
 A execução de comandos SSH de um programa C pode ser feita usando a biblioteca libssh. Primeiro, instale a biblioteca libssh em seu sistema, se ainda não o fez. Aqui está um exemplo de código que se conecta a um servidor SSH, autentica-se e executa os comandos desejados:
 
 Substitua your_remote_server_ip, your_remote_username, your_remote_server_port e your_remote_password pelos valores apropriados. Por razões de segurança, é recomendável usar a autenticação por chave em vez de senha. Para isso, você pode substituir a chamada de função ssh_userauth_password por ssh_userauth_publickey_auto e remover a opção de senha.
@@ -8,3 +7,4 @@ Compile e execute o programa com as seguintes instruções:
 gcc -o ssh_commands ssh_commands.c -lssh
 ./ssh_commands
 
+sudo apt-get install libssh-dev
--- ssh/02/main.c
+++ ssh/02/main.c
@@ -1,21 +1,91 @@
+#include <libssh2.h>
 #include <stdio.h>
-#include <curl/curl.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
 
-int main(void)
-{
-  CURL *curl;
-  CURLcode res;
-  
-  curl = curl_easy_init();
-  if(curl) {
-    curl_easy_setopt(curl, CURLOPT_URL, "http://192.168.2.43:8899/");
-    res = curl_easy_perform(curl);
-    
-    if(res != CURLE_OK)
-      fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
-      
-    curl_easy_cleanup(curl);
-  }
-  
-  return 0;
+void execute_ssh_command(LIBSSH2_SESSION *session, const char *command) {
+    LIBSSH2_CHANNEL *channel;
+    int rc;
+    char buffer[256];
+    ssize_t nbytes;
+
+    channel = libssh2_channel_open_session(session);
+    if (!channel) {
+        exit(-1);
+    }
+
+    rc = libssh2_channel_exec(channel, command);
+    if (rc != 0) {
+        libssh2_channel_free(channel);
+        exit(-1);
+    }
+
+    nbytes = libssh2_channel_read(channel, buffer, sizeof(buffer));
+    while (nbytes > 0) {
+        fwrite(buffer, 1, nbytes, stdout);
+        nbytes = libssh2_channel_read(channel, buffer, sizeof(buffer));
+    }
+
+    libssh2_channel_send_eof(channel);
+    libssh2_channel_wait_eof(channel);
+    libssh2_channel_wait_closed(channel);
+    libssh2_channel_free(channel);
+}
+
+int main() {
+    const char *hostname = "192.168.2.43";
+    const char *username = "ivan";
+    const char *password = "XXXXXXXXX";
+    int port = 22;
+
+    int sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock == -1) {
+        perror("socket");
+        exit(-1);
+    }
+
+    struct sockaddr_in sin;
+    sin.sin_family = AF_INET;
+    sin.sin_port = htons(port);
+    sin.sin_addr.s_addr = inet_addr(hostname);
+    if (connect(sock, (struct sockaddr *)(&sin), sizeof(struct sockaddr_in)) != 0) {
+        perror("connect");
+        exit(-1);
+    }
+
+    LIBSSH2_SESSION *session = libssh2_session_init();
+    if (!session) {
+        exit(-1);
+    }
+
+    int rc = libssh2_session_handshake(session, sock);
+    if (rc != 0) {
+        fprintf(stderr, "Error during SSH handshake: %d\n", rc);
+        exit(-1);
+    }
+
+    rc = libssh2_userauth_password(session, username, password);
+    if (rc != 0) {
+        fprintf(stderr, "Error during SSH authentication: %d\n", rc);
+        exit(-1);
+    }
+
+    printf("Executing 'pwd' command:\n");
+    execute_ssh_command(session, "pwd");
+
+    printf("\nExecuting 'ls' command:\n");
+    execute_ssh_command(session, "ls");
+
+    printf("\nExecuting 'gcc -v' command:\n");
+    execute_ssh_command(session, "gcc -v");
+
+    libssh2_session_disconnect(session, "Normal Shutdown");
+    libssh2_session_free(session);
+
+    close(sock);
+
+    return 0;
 }
