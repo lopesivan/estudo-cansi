ac321a7ab7f5af187005349012bb2043d3de05c5
Ivan Lopes <lopesivan.ufrj@gmail.com>
2025-08-17 12:38:20 -0300

    :style: Atualiza o estilo do arquivo mono/03/autogen.sh

--- mono/03/autogen.sh
+++ mono/03/autogen.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
 mkdir -p build-aux m4
+
 libtoolize --install --copy # no macOS use: glibtoolize --install --copy
 aclocal -I m4
 autoconf
 automake --add-missing --copy
 
+./configure && make && make -C src run
 exit 0
